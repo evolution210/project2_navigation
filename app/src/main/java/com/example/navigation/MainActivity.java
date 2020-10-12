044698b4dad371bbf6bc309ce44e38e6e4e4002c
evolution210 <dpwlsghk@naver.com>
2020-10-12 16:23:26 +0900

    activity_xml 생성
    4개

--- app/src/main/java/com/example/navigation/MainActivity.java
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void Click(View v){// 비콘 수신 기능 개발 전 다음 페이지로 넘어가기 위한 버튼 (Activity 이동)
+        Intent intent = new Intent(MainActivity.this,select_destination.class);
+        //intent.putExtra("category",);//category 넘겨주기
+        startActivity(intent);
     }
 }
